e9a0167c2f09f87e58bdc6cf9d9657e0fa997a89
thinhngo <ngothinh15111991@gmail.com>
2026-01-22 08:16:28 +0700

    hoàn thành asm 1

---
+++ TS01533-Ngo Phuoc Thinh-VueJS/Lab4/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Hello from './components/Hello.vue';
+import bai3 from './components/bai3.vue';
+import bai4 from './components/bai4.vue';
+</script>
+
+<template>
+  <bai4 />
+</template>
+
+<style scoped>
+</style>
---
+++ TS01533-Ngo Phuoc Thinh-VueJS/Lab4/src/components/demo.vue
@@ -0,0 +1,19 @@
+<script>
+const yob = ref(0);
+let age = ref(0)
+const calcu = () => (age = 2026 - yob.value);
+</script>
+
+<template>
+    <div class = "container">
+        <div class="mt-5">
+            <input type="number" v-model="yob" min="1800" max="2026">
+
+            <div class="">{{ calcu() }}</div>
+
+            <p v-if = "age >= 15 && age < 18">Thiếu niên</p>
+            <p v-else-if = "age >=18">Trẻ tuổi</p>
+            <p v-else>Chưa thành niên</p>
+        </div>
+    </div>
+</template>
